a08d614edef0e7fbf72c9729c501053720cdc63e
Brooklynm123 <brklnmcdnld@gmail.com>
2025-05-31 12:03:16 -0400

    added my attributes and my defult values

--- Gme1011a2BrooklynM/Potion.cs
+++ Gme1011a2BrooklynM/Potion.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Gme1011a2BrooklynM
 {
     internal class Potion
     {
+        private Texture2D _potionTexture;
+        private Vector2 _location;
+        private int _healamount;
+        Random _rng = new Random();
+        private bool _drink;
     }
+    public Potion(Vector2 location,Texture2D potionTexture)
+        {
+        _location = location;
+        _potionTexture = potionTexture;
+        _healamount = _rng.Next(5, 10);
+        _drink = false;
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
 }
+
+
